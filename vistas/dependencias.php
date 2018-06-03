b8407e6762d4ab06c6677c9c52e651deb38c44b3
Alan Amaury Escobedo García <amaury.escobedo29@gmail.com>
2018-06-03 18:35:22 -0500

    Se vinculan las dependencias y se agrega el archivo menu.php

---
+++ vistas/dependencias.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../librerias/alertifyjs/css/alertify.css">
+<link rel="stylesheet" href="../librerias/alertifyjs/css/themes/default.css">
+<link rel="stylesheet" href="../librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="../librerias/select2/css/select2.css">
+
+<script src="../librerias/jquery-3.3.1.js"></script>
+<script src="../librerias/alertifyjs/alertify.js"></script>
+<script src="../librerias/bootstrap/js/bootstrap.js"></script>
+<script src="../librerias/select2/js/select2.js"></script>
