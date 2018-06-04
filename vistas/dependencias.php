24e53433cf7fdbac2d8e523544666d1e8fce6f25
Alan Amaury Escobedo García <amaury.escobedo29@gmail.com>
2018-06-03 22:01:48 -0500

    Se crea el menú para entrar a la sección correspondiente

--- vistas/dependencias.php
+++ vistas/dependencias.php
@@ -2,6 +2,7 @@
 <link rel="stylesheet" href="../librerias/alertifyjs/css/themes/default.css">
 <link rel="stylesheet" href="../librerias/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" href="../librerias/select2/css/select2.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
 <script src="../librerias/jquery-3.3.1.js"></script>
 <script src="../librerias/alertifyjs/alertify.js"></script>
--- vistas/menu.php
+++ vistas/menu.php
@@ -8,5 +8,41 @@
     <?php require_once "dependencias.php"; ?>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><i class="fas fa-home"></i> Inicio</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-boxes"></i> Administrador de Artículos
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#"><i class="fas fa-tags"></i> Categorías</a>
+                    <a class="dropdown-item" href="#"><i class="fas fa-box"></i> Artículos</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-users"></i> Administrador de Usuarios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-user"></i> Clientes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-piggy-bank"></i> Vender Artículo</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-circle"></i> Usuario: 
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Salir</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+
 </body>
 </html>
